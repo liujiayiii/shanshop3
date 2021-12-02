79602c906dd6a1597f5e9d51b765da1fcb05e30d
liujiayi <163895609@qq.com>
2021-12-02 23:57:05 +0800

    刘第三次提交

--- shanshop3/src/components/views/login.vue
+++ shanshop3/src/components/views/login.vue
@@ -1,24 +1,33 @@
 <template>
+
   <body id="poster">
+  <el-page-header @back="goBack" content="登录页面" style="color: white">
+  </el-page-header>
+  <div class="login-wrap">
   <el-form class="login-container"
            label-position="left" label-width="0px">
     <h2 class="login-title">用  户  登  录</h2>
     <br>
-    <el-form-item>
-    用户名：<el-input type="text"
+    <el-form-item style="color: white">
+    用户名：<el-tooltip class="item" effect="dark" content="Top Center 提示文字" placement="top">
+      <el-input type="text"
                   v-model="loginForm.username"
                   placeholder="请输入用户名"
                   style="width: 80%;text-align: center">
-      <i slot="prefix" class="el-icon-user"></i>
+      <i slot="prefix" class="el-icon-user" style="color: white"></i>
     </el-input>
+    </el-tooltip>
     </el-form-item>
-    <el-form-item>
-    密码： <el-input type="password"
+    <el-form-item style="color: white">
+      密码：
+      <el-tooltip class="item" effect="dark" content="Top Center 提示文字" placement="top">
+     <el-input type="password"
                v-model="loginForm.password"
                placeholder="请输入密码"
                   style="width: 80%;text-align: center">
-      <i slot="prefix" class="el-icon-unlock"></i>
+      <i slot="prefix" class="el-icon-unlock" style="color: white"></i>
     </el-input>
+      </el-tooltip>
     </el-form-item>
     <br>
     <el-form-item style="width: 100%" >
@@ -27,10 +36,11 @@
                v-on:click="login">登 录</el-button>
     </el-form-item>
     <el-form-item style="text-align: right; width: 100%">
-      <el-link @click="gotoRegister()">没有注册？</el-link>
+      <el-link @click="gotoRegister()" type="primary" style="color: white">没有注册？</el-link>
       <!--      增加跳转注册页面的功能-->
     </el-form-item>
   </el-form>
+  </div>
   </body>
 </template>
 
@@ -44,19 +54,22 @@
         loginForm:{
           username:'admin',
           password:'admin123'
-        },
-
+        }
       }
     },
     methods:{
+      goBack() {
+        this.$router.push({
+          name: 'sindex'
+        })
+      },
       gotoRegister(){
         this.$router.push({
           name: 'register'
         })
       },
       login () {
-        axios
-          .post('http://118.31.250.71:8428/login',qs.stringify(this.loginForm) )
+        axios.post('http://118.31.250.71:8428/login',qs.stringify(this.loginForm) )
           .then(successResponse => {
             console.log(successResponse)
             if (successResponse.status ===200){
@@ -71,29 +84,32 @@
   }
 </script>
 
-<style scoped>
+<style >
+  .el-input__inner{
+    background: transparent;
+  }
 .login-container{
   border-radius: 15px;
   /*给元素边角设置圆角弧度*/
   background: padding-box;
   /*确定背景定位*/
-  margin: 220px auto;
+  margin: 100px auto;
   /*所有外边距*/
   width: 350px;
   padding: 35px 35px 15px 35px;
-  background:#ffffff;
-  border: 1px solid #173c82;
-  box-shadow: 0 0 25px #173c82;
+  background:transparent;
+  border: 1px solid #ffffff;
+  box-shadow: 0 0 25px #ffffff;
   text-align: center;
 }
-.login_title {
+.login-title {
   margin: 0px auto 40px auto;
   text-align: center;
   /*对齐方式*/
-  color: #505458;
+  color: #ffffff;
 }
 #poster {
-  background:url("../../assets/k.jpg") no-repeat;
+  background:url("../../assets/5cd154cef1b12.jpg") no-repeat;
   background-position: center;
   height: 100%;
   width: 100%;
--- shanshop3/src/components/views/register.vue
+++ shanshop3/src/components/views/register.vue
@@ -1,13 +1,86 @@
 <template>
-  <div> hello!</div>
+  <div class="login-wrap">
+    <el-form class="login-container"
+             label-position="left"
+             label-width="0px">
+      <h2 class="title">用 户 注 册</h2>
+    <el-form-item>
+    <el-input type="text"
+              placeholder="请输入用户名"
+              v-model="loginForm.username"
+              autocomplete="off"
+              style="width: 100%"
+              class="user"
+              maxlenngth="10"
+              show-word-limit></el-input>
+    </el-form-item>
+      <el-form-item>
+        <el-input type="password"
+                  placeholder="请输入密码"
+                  v-model="loginForm.password"
+                  autocomplete="off"
+                  style="width: 100%;"></el-input>
+      </el-form-item>
+      <el-form-item label="">
+        <el-input type="password"
+                  placeholder="确认密码"
+                  v-model="loginForm.password1"
+                  autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="doRegister()" style="width: 48%">用户注册</el-button>
+        <el-button @click="gotoLogin()" style="width: 48%">返回登陆</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "register"
+        name: "register",
+      data(){
+          return{
+            loginForm:{
+              username:null,
+              password:null,
+              password1:null
+            }
+          }
+      },
+      methods:{
+          gotoLogin(){
+            this.$router.push('/login');
+          },
+
+      }
     }
 </script>
 
-<style scoped>
+<style >
+  .el-input__inner{
+    background: transparent;
+  }
+.login-wrap{
+  box-sizing: border-box;
+  width: 100%;
+  height:100%;
+  background-image: url("../../assets/s.jpg");
+  background-repeat: no-repeat;
+  background-position: center right;
+  background-size: 100%;
+}
+  .login-container{
+    border-radius: 10px;
+    margin: 0px auto;
+    width: 350px;
+    padding: 30px 35px 15px 35px;
+    background-image: url("../../assets/k.jpg");
 
+  }
+  .title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    /*对齐方式*/
+    color: #000000;
+  }
 </style>
